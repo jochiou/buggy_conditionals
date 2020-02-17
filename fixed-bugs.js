63bffc4b6ac6d9282195e1742e13f62fdae183ea
Joseph Chiou <jochiou0601@gmail.com>
2020-02-17 14:21:17 -0800

    Added PuttingItAllTogether + Remote Repositories

---
+++ fixed-bugs.js
@@ -0,0 +1,23 @@
+var firstName = "Robert";
+var nickName = "Bob";
+var petName = "Fluffy";	// Fluffy is not defined, change Fluffy to "Fluffy" to make this statment a string assignment
+var petType = "Turtle"; // change xar to var
+if(firstName == "Robert"){	// name is not defined, should be firstName
+    console.log("Hi Robert");
+}
+else if(nickName == "Bob"){	// name is not defined, should be nickname
+    console("Hi Bob");
+}
+else if(name != "Robert"){	// change else (xxx) to else if (xxx)
+    console.log("Hello Stranger");
+}
+if (petType == "Dog"){		// add paranthesis for the if statment
+    console.log("Time to play fetch");
+}
+if(petType = "Cat"){
+    console.log("Time to play with a laser pointer");
+}
+else{
+    console.log("Time to play with my pet");  // add quotation makrs for string arguement
+}
+
